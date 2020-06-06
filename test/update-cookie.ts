d2bfe07b721524abafd65574e5cc99ab8d94a5c2
masx200 <masx200@qq.com>
2020-06-06 22:58:59 +0800

    优化

--- test/update-cookie.ts
+++ test/update-cookie.ts
@@ -1,6 +1,6 @@
 import { gethomehtmlandupdatecookie } from "../lib/gethomehtmlandupdatecookie.js";
 const htmldata = gethomehtmlandupdatecookie();
-htmldata.then((html) => console.log(html.slice(0, 100000)));
+htmldata.then((html) => console.log(html.slice(0, 10000)));
 process.on("unhandledRejection", (err) => {
     throw err;
 });
