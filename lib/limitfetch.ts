4e43f968e2aec8df58eb166342b5edfde0a990ef
masx200 <masx200@qq.com>
2020-06-27 16:06:18 +0800

    升级依赖,修复https的keepalive功能

--- lib/limitfetch.ts
+++ lib/limitfetch.ts
@@ -2,17 +2,17 @@ import currentlimiter from "@masx200/async-task-current-limiter";
 import https from "https";
 import fetch from "node-fetch";
 const fetchlimiter = currentlimiter(15);
-
+const agent = new https.Agent({
+    keepAlive: true,
+});
+https.globalAgent = agent;
 const limitedfetch = fetchlimiter.asyncwrap(function (
     url: string,
     opt?: RequestInit
 ): Promise<Response> {
-    const agent = new https.Agent({
-        keepAlive: true,
-    });
     // @ts-ignore
     return fetch(url, {
-        agent: url.startsWith("http:") ? undefined : agent,
+        agent: url.startsWith("https:") ? agent : undefined,
         ...opt,
     });
 });
