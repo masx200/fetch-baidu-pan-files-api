7dd370499c4306587e76439deabfd315246e2892
masx200 <34191203+masx200@users.noreply.github.com>
2020-06-29 00:37:33 +0800

    Update response-error-handler.ts

--- lib/response-error-handler.ts
+++ lib/response-error-handler.ts
@@ -1,7 +1,7 @@
 import 错误码表 from "./errno.js";
-
+import assert from "assert";
 export function response_error_handler(data:any,urlhref:string){
-
+assert(typeof data==="object"&& data && !Array.isArray(data))
 
 const errno = data?.errno;
                 assert(typeof errno === "number");
