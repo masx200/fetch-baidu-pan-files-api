d84bcaa646c3ce6145b626f5d83ae35cce690db9
masx200 <34191203+masx200@users.noreply.github.com>
2020-07-01 18:44:38 +0800

    Update parse-cookie-save.ts

--- cli/parse-cookie-save.ts
+++ cli/parse-cookie-save.ts
@@ -1,12 +1,12 @@
 import cookie from "cookie";
 import fsextra from "fs-extra";
 // const jsonfile = "./cookies.json";
-import { jsonfile, txtfile } from "./files.js";
+import { jsonfile, txtfile } from "../lib/files.js";
 
 //const __filename = fileURLToPath(import.meta.url);
 //const __dirname = dirname(__filename);
 //const txtfile = path.join(__dirname, "./cookies.txt");
-export const parsecookie = async () => {
+export const parsecookiesave = async () => {
     const buf = await fsextra.readFile(txtfile);
     const cookiestr = buf.toString();
     console.log(cookiestr);
