9c7d9222dee6e27d59f42ac4b47c4ab53f6e3818
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-25 22:22:45 +0800

    优化

--- cli/parse-cookie-save.ts
+++ cli/parse-cookie-save.ts
@@ -6,7 +6,7 @@ import { jsonfile } from "../lib/files.js";
 export const parsecookiesave = async (cookiestr: string) => {
     // const buf = await fsextra.readFile();
     // const cookiestr = buf.toString();
-    console.log(cookiestr);
+    // console.log(cookiestr);
     const parsedobj = cookie.parse(cookiestr, {});
     console.log(parsedobj);
     await fsextra.writeJSON(jsonfile, parsedobj, { spaces: 4 });
